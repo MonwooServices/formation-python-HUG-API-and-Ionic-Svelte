7fbeea3dbe1b32d0fb1f4b79cf3913ca93163411
ElvexDom <n.bertrand@elvex-domotique.fr>
2023-07-31 13:25:11 +0200

    [FORMATION] msg hug post

--- serveur-api/infrastructure/db_msg.py
+++ serveur-api/infrastructure/db_msg.py
@@ -1,12 +1,12 @@
 from peewee import *
 from datetime import *
-from db_peewee import message
+from db_peewee import Message
 
-message.create(
+Message.create(
     date=datetime.today().strftime('%Y-%m-%d'),
     time=datetime.today().strftime('%H:%M:%S'),
     msg="log",
     haveBeenSaid='Hello, i am buddy')
-query = message.select()
-for message in query:
-    print(message.date, message.time, message.msg, message.haveBeenSaid)
+query = Message.select()
+for Message in query:
+    print(Message.date, Message.time, Message.msg, Message.haveBeenSaid)
