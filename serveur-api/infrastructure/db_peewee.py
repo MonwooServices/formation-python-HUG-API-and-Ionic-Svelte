44c0910517d457ca693b5cb82f8ca5cd8ae763dc
ElvexDom <n.bertrand@elvex-domotique.fr>
2023-07-31 16:30:19 +0200

    [FORMATION] Ajout d'un bdd user-api.db.sqlite

--- serveur-api/infrastructure/db_peewee.py
+++ serveur-api/infrastructure/db_peewee.py
@@ -1,15 +1,64 @@
 from peewee import *
+from datetime import *
 
-db = SqliteDatabase('buddy-api.db.sqlite')
+# This is the database repertory
+db_rep = "serveur-api/infrastructure/database/"
 
+# This model uses the "buddy-api.db.sqlite" database
+db_buddy = SqliteDatabase(db_rep +'buddy-api.db.sqlite')
 class Message(Model):
     date = DateField()
     time = DateField()
     msg = CharField()
-    haveBeenSaid = CharField()
-
     class Meta:
-        database = db # This model uses the "people.db" database.
+        database = db_buddy
 
-db.connect()
-db.create_tables([Message])
+# This model uses the "user-api.db.sqlite" database
+db_user = SqliteDatabase(db_rep +'user-api.db.sqlite')
+class User(Model):
+    identity = CharField()
+    name = CharField()
+    password = CharField()
+    key = CharField()
+    class Meta:
+        database = db_user
+
+# Creating table "Message" in "buddy-api.db.sqlite" database
+def create_table_msg_buddy():
+    db_buddy.connect()
+    db_buddy.create_tables([Message])
+    db_buddy.close()
+
+# Creating data in table "Message"
+def create_msg_buddy(var_msg):
+    db_buddy.connect()
+    Message.create(
+        date=datetime.today().strftime('%Y-%m-%d'),
+        time=datetime.today().strftime('%H:%M:%S'),
+        msg=var_msg
+        )
+    db_buddy.close()
+
+# Creating table "User" in "user-api.db.sqlite" database
+def create_table_user_buddy():
+    db_user.connect()
+    db_user.create_tables([User])
+    db_user.close()
+
+# Creating data in table "User"
+def create_user_buddy(var_name):
+    db_user.connect()
+    User.create(
+        identity="ec6a3dae-0278-49d5-b909-03846369bae4",
+        name=var_name,
+        password="1234",
+        key="HWeC6Tp4hTxXGPStfJgjoirj3XdzPgfBuf6CbRf8xF3Iacz4f7di1taQf6a05tlR="
+        )
+    db_user.close()
+
+### Commands test ###
+
+#create_table_msg_buddy()
+#create_table_user_buddy()
+#create_msg_buddy("buddy")
+#create_user_buddy("nicolas")
