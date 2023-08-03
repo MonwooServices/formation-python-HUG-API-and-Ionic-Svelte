ba1b84f642ced440b7746b56b9b75c0b6faeecd6
ElvexDom <n.bertrand@elvex-domotique.fr>
2023-08-03 12:42:36 +0200

    [FORMATION] Installation svelte

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import { onMount } from 'svelte';
+	//onMount(async await fetch(`http://${backendIp}:${backendPort}/api/buddy/read-msgs`)
+	onMount(async() => {
+		const queryString = window.location.search;
+		const urlParams = new URLSearchParams(queryString);
+		const backendIp = urlParams.get("backendIp") || "86.193.154.93";
+		const backendPort = urlParams.get("backendPort") || "8000";
+		await fetch(`http://${backendIp}:${backendPort}/api/buddy/read-msgs`);
+		console.log('the component has mounted');
+});
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to<br />buddy application
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
